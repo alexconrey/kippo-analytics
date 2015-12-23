18ffc5843b1a9652bccc91df52dfad055ff9481c
alex <alex@conrey.us>
2015-12-23 14:52:05 -0600

    added search to searchByIP.php

--- searchByIP.php
+++ searchByIP.php
@@ -50,7 +50,24 @@ while($attempt = $result->fetch_assoc()) {
 <?php
 
 } else {
-echo 'please specify an IP with a GET request';
-}
+?>
+<div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Search By IP</h1>
+          <div class="col-lg-6">
+          	<div class="input-group">
+          		<form action="#" method="get">
+          		<input type="text" class="form-control" name="ip" placeholder="Search for...">
+          		<span class="input-group-btn">
+          			<button class="btn btn-default" type="submit">Go!</button>
+          		</span>
+          	</div>
+          </div>
+        </div>
+      </div>
+</div>
 
+<?php
+}
 ?>
--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,6 @@
             <li class="active"><a href="index.php">Dashboard <span class="sr-only">Dashboard</span></a></li>
             <li><a href="listSensors.php">Sensor List</a></li>
             <li><a href="popularPasswords.php">Popular Passwords</a></li>
-            <li><a href="#">Export</a></li>
           </ul>
           <ul class="nav nav-sidebar">
             <li><a href="listSessions.php">List Sessions</a></li>
@@ -13,10 +12,5 @@
             <li><a href="searchByID.php">Search By ID</a></li>
             <li><a href="searchByUser.php">Search By Username</a></li>
             <li><a href="searchBySensor.php">Search by Sensor</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
           </ul>
         </div>
