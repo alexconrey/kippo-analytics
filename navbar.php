a6df365c99d8dd4343b176f66ebe20215597d211
Alex Conrey <aconrey@vailsys.com>
2015-12-30 12:12:35 -0600

    navbar

--- navbar.php
+++ navbar.php
@@ -11,13 +11,11 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
+            <li><a href="index.php">Dashboard</a></li>
             <li><a href="#">Help</a></li>
           </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
+          <form class="navbar-form navbar-right" action="search.php" method="get">
+            <input type="text" class="form-control" placeholder="Search..." name="keyword">
           </form>
         </div>
       </div>
